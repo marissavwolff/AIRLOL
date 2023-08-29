d2c28751f6813fb48d428395e7b601858f40720f
Charlotte Reyburn <charlottereyburn7@gmail.com>
2023-08-29 14:40:45 +0100

    booking

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :listings
   has_many :orders
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :number, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :listings, only: [:index, :new, :show, :create]
-
+  resources :bookings, only: [:index]
 
   # Defines the root path route ("/")
   # root "articles#index"
