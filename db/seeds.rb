703e0e47530f8f5fb621f24792a7eba14484d689
madeleine plumridge <madeleine.plumridge@gmail.com>
2023-08-29 14:40:11 +0200

    Update show listing page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,14 @@ require 'faker'
 Listing.destroy_all
 
 puts "doing seed here"
-User.create!(name: "Tom", number: "+33456 781345", email: "tom@lewagon", password: "kafjgalkjdfhgbald")
+User.create!(name: "Tom", number: "+33456 781345", email: "tim@lewagon", password: "kafjgalkjdfhgbald")
 
 20.times do
+  name = ['Make you friends cry of laughter', 'Win them all over with this humour', 'A joke a day', 'Laugh all day', 'LOL al; day']
   location = ['London', 'Melbourne', 'Lausanne', 'Milan', 'France']
   priceArray = [1, 2, 4, 56, 87, 23, 676, 23, 57, 54, 32]
   myArray = ["witty", "dark", "dry", "slapstick", "family", "puns", "self-depreciating", "toilet", "satire", "awkward", "cringe"]
-  Listing.create!(category: myArray.sample, description: Faker::Quote.jack_handey, price: priceArray.sample, location: location.sample, user_id: 1)
+  Listing.create!(name: name.sample, category: myArray.sample, description: Faker::Quote.jack_handey, price: priceArray.sample, location: location.sample, user_id: 1)
 end
 
 puts "created seeds"
