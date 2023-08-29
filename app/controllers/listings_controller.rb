f0216e289b00b758874e7a9995fda33e240811bc
Marissa vonWolff <marissa.wolff@gmx.de>
2023-08-29 13:01:09 +0200

    create listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,33 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :destroy]
+
   def show
-    @listing = Listing.find(params[:id])
   end
 
   def index
     @listings = Listing.all
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :category, :location, :availability, :description, :price )
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
   end
 end
