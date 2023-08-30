ec06bc7230618a12029cef687433e1301179c725
madeleine plumridge <madeleine.plumridge@gmail.com>
2023-08-30 11:33:11 +0200

    Add gotenv-rails

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,7 @@ class Listing < ApplicationRecord
   validates :category, inclusion: { in: ["witty", "dark", "dry", "slapstick", "family", "puns", "self-depreciating", "toilet", "satire", "awkward", "cringe"] }
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
